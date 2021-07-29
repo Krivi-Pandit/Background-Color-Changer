8c908f5743f9d9af9338f546f423d8d1596cd2a5
Krivi-Pandit <88101465+Krivi-Pandit@users.noreply.github.com>
2021-07-29 19:07:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  if(keyIsDown(DOWN_ARROW))
+  { 
+    background('red');
+  }
+  if(keyIsDown(UP_ARROW))
+  { 
+    background('green');
+  }
+  if(keyIsDown(RIGHT_ARROW))
+  { 
+    background('blue');
+  }
+  if(keyIsDown(LEFT_ARROW))
+  { 
+    background('yellow');
+  }
+  drawSprites()
+}
+
+
+
